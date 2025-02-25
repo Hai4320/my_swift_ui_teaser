77ad934b21ec6963cdb1e16e3458407977171538
haihd-3154 <ho.duc.hai-b@sun-asterisk.com>
2025-02-25 17:38:32 +0700

    Currentcy converter app

---
+++ HariConverter/HariConverter/HariConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HariConverterApp.swift
+//  HariConverter
+//
+//  Created by ho.duc.haib on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HariConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
