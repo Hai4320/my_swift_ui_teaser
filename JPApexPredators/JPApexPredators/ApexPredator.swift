d775c4002c6a19ec6ec141ffe52327b1c942f2a3
haihd-3154 <ho.duc.hai-b@sun-asterisk.com>
2025-03-10 15:51:13 +0700

    Init Apex predator

---
+++ JPApexPredators/JPApexPredators/ApexPredator.swift
@@ -0,0 +1,16 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by ho.duc.haib on 3/10/25.
+//
+struct ApexPredator {
+    let id: Int
+    let name: String
+    let type: String
+    let lastitude: String
+    let longitude: String
+    
+    
+    
+}
---
+++ JPApexPredators/JPApexPredators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredators
+//
+//  Created by ho.duc.haib on 3/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
